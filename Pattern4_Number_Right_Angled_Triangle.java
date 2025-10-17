4a983db7599a15db150bc683c48f27ed57186f02
Kumaran R <143732898+KumaranR18@users.noreply.github.com>
2025-10-17 20:37:34 +0530

    Add files via upload

---
+++ Pattern4_Number_Right_Angled_Triangle.java
@@ -0,0 +1,24 @@
+Pattern-4 Number Right-Angled Triangle
+
+Enter the value of n: 5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+
+import java.util.*;
+class Pattern4_Number_Right_Angled_Triangle{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= n; col++){
+                System.out.print(row+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}

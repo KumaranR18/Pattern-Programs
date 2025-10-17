cbbdf2eca30f44f11d3a97142129ca09a5df43b8
Kumaran R <143732898+KumaranR18@users.noreply.github.com>
2025-10-17 20:49:12 +0530

    Add files via upload

---
+++ Pattern5_Binary_Right_Angled_Triangle_Pattern.java
@@ -0,0 +1,24 @@
+Pattern-5 Binary Right-Angled Triangle Pattern
+
+Enter the value of n: 5
+1 
+0 1 
+1 0 1 
+0 1 0 1 
+1 0 1 0 1 
+
+
+import java.util.*;
+class Pattern5_Binary_Right_Angled_Triangle_Pattern{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= n; col++){
+                System.out.print(row+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}

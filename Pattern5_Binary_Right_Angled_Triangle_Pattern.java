fcee7631ad7122151dc72bcb295407ab5674a012
KumaranR18 <143732898+KumaranR18@users.noreply.github.com>
2025-10-17 21:11:58 +0530

    Add files via upload

--- Pattern5_Binary_Right_Angled_Triangle_Pattern.java
+++ Pattern5_Binary_Right_Angled_Triangle_Pattern.java
@@ -10,15 +10,18 @@ Enter the value of n: 5
 
 import java.util.*;
 class Pattern5_Binary_Right_Angled_Triangle_Pattern{
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n: ");
         int n = sc.nextInt();
+        int printVal;
         for(int row = 1; row <= n; row++){
-            for(int col = 1; col <= n; col++){
-                System.out.print(row+" ");
+        	 printVal = row % 2 == 0 ? 0 : 1;
+            for(int col = 1; col <= row; col++){
+                System.out.print(printVal+" ");
+                printVal = printVal == 0 ? 1 : 0;
             }
-            System.out.print("\n");
+            System.out.println();
         }
     }
 }
